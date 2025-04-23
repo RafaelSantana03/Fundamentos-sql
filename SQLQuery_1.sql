caefd8c7baeb6ce5a092cfe79ce50a60a55b44f3
Rafael Santana <rafa03santana@gmail.com>
2025-04-23 20:36:55 -0300

    Compose key

--- SQLQuery_1.sql
+++ SQLQuery_1.sql
@@ -1,15 +1,49 @@
 USE [Curso]
 
+DROP TABLE [Aluno]
 --Criando a tabela aluno
 CREATE TABLE [Aluno](
     --Inserindo dados na tabela
-    [Id] INT NOT NULL, 
+    [Id] INT NOT NULL,
     [Nome] NVARCHAR(80) NOT NULL,
+    [Email] NVARCHAR(180) NOT NULL , --O comando "UNIQUE" Não permite que tenham dois "Id" de mesmo valor
     [Nascimento] DATETIME NULL,
-    [Active] BIT,
+    [Active] BIT NOT NULL DEFAULT(0),
+
+    CONSTRAINT[PK_Aluno] PRIMARY key([Id]), -- Nomeando chaves primarias e unicas
+    CONSTRAINT[UQ_Aluno_Email] UNIQUE([Email]),
 )
 GO
 
--- Fazendo alteração na tabela Aluno para tornar o campo Active obrigatório (NOT NULL)
-ALTER TABLE[Aluno]
-    ALTER COLUMN [Active] BIT NOT NULL
+DROP TABLE [Curso]
+CREATE TABLE [Curso](    
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+    [CategoriaId]  INT NOT NULL,
+
+    CONSTRAINT[PK_Curso] PRIMARY key([Id]), 
+    CONSTRAINT[FK_Curso_Categoria] FOREIGN KEY([CategoriaId])
+        REFERENCES [Categoria]([Id])
+)
+GO
+
+CREATE TABLE [Categoria](    
+    [Id] INT NOT NULL,
+    [Nome] NVARCHAR(80) NOT NULL,
+
+    CONSTRAINT[PK_Categoria] PRIMARY key([Id]), 
+)
+GO
+
+DROP TABLE [ProgressoCurso]
+--Criando uma tabela asociativa que liga as duas tabelas
+CREATE TABLE [ProgressoCurso](    
+    [AlunoId] INT NOT NULL,
+    [CursoId] INT NOT NULL,
+    [Progresso] INT NOT NULL,
+    [UltimaAtualização] DATETIME NOT NULL DEFAULT(GETDATE()),
+
+    CONSTRAINT PK_ProgressoCurso PRIMARY KEY ([AlunoId], [CursoId]),
+)
+GO
+
---
+++ SQLQuery_2.sql
@@ -0,0 +1,6 @@
+-- ALTER TABLE[Aluno]
+    -- DROP CONSTRAINT [PK_Aluno]
+
+-- Fazendo alteração na tabela Aluno para tornar o campo Active obrigatório (NOT NULL)
+-- ALTER TABLE[Aluno]
+   -- ALTER COLUMN [Active] BIT NOT NULL

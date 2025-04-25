39134476aa8c92e72fb42aee1a013d93d09cd382
Rafael Santana <rafa03santana@gmail.com>
2025-04-24 23:25:31 -0300

    Update, Delete, MIN, MAX, COUNT e Like

---
+++ Like.sql
@@ -0,0 +1,28 @@
+SELECT TOP 100
+   *
+FROM 
+    [Curso]
+WHERE
+    [Nome] LIKE '% de %'
+
+
+-- SELECT * FROM [Curso]
+
+--     UPDATE --Atualizando dados da tabela [Categoria]
+--         [Categoria]
+--     SET
+--         [Nome] = 'Azure'
+--     WHERE
+--         [Id] = 4
+
+-- SELECT * FROM [Curso]
+
+-- DELETE FROM
+--     [Curso]
+-- WHERE
+--     [CategoriaId] = 3
+
+-- DELETE FROM
+--     [Categoria]
+-- WHERE
+--     [Id] = 3

e078e1b8ead117d8cef28dda29106b63f820a85f
Rafael Santana <rafa03santana@gmail.com>
2025-05-09 08:09:05 -0300

    INNER JOIN

--- Like.sql
+++ Like.sql
@@ -1,9 +1,18 @@
+-- Seleciona os 100 primeiros resultados da consulta
 SELECT TOP 100
-   *
+   [Curso].[Id],                      -- Seleciona a coluna 'Id' da tabela Curso
+   [Curso].[Nome],                    -- Seleciona a coluna 'Nome' da tabela Curso
+   [Categoria].[Id] AS [Categoria],   -- Seleciona o 'Id' da Categoria, mas renomeia (alias) como 'Categoria'
+   [Categoria].[Nome]                 -- Seleciona o 'Nome' da Categoria
 FROM 
     [Curso]
-WHERE
-    [Nome] LIKE '% de %'
+    -- Faz a junção da tabela Curso com a tabela Categoria
+    INNER JOIN [Categoria]
+        -- Condição da junção: o campo CategoriaId da tabela Curso deve ser igual ao Id da Categoria
+        ON [Curso].[CategoriaId] = [Categoria].[Id]
+
+
+    --[Id] IN (1, 2)
 
 
 -- SELECT * FROM [Curso]
